5926b5ddf3faa63cfc0f770dff2d69d845a5e968
syk_09@naver.com <syk_09@naver.com>
2020-05-31 16:09:54 +0900

    Add : prototype player by Anima2D

---
+++ Hunting Season/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField] float time = 0;
+    [SerializeField] float dayTime;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time >= dayTime)
+        {
+            time = 0;
+        }
+    }
+}
